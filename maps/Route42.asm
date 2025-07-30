f91d2fde29b936c756103df042cce16998daf2eb
That-Fox <abigail.marie.jonson@gmail.com>
2025-07-30 05:26:16 -0400

    Update Route42.asm
    Modified Lyra to reflect changes elsewhere

--- maps/Route42.asm
+++ maps/Route42.asm
@@ -103,19 +103,15 @@ Route42LyraScript:
 	setevent EVENT_LYRA_ROUTE_42
 	winlosstext Route42LyraWinText, Route42LyraLossText
 	setlasttalked ROUTE42_LYRA
-	checkevent EVENT_GOT_TOTODILE_FROM_ELM
-	iftruefwd .Totodile
-	checkevent EVENT_GOT_CHIKORITA_FROM_ELM
-	iftruefwd .Chikorita
-	loadtrainer LYRA1, LYRA1_7
+	checkevent EVENT_GOT_EEVEE_FROM_ELM
+	iftruefwd .Johto
+	loadtrainer LYRA1, LYRA1_5
 	sjumpfwd .AfterBattle
 
-.Totodile:
-	loadtrainer LYRA1, LYRA1_8
+.Johto:
+	loadtrainer LYRA1, LYRA1_6
 	sjumpfwd .AfterBattle
 
-.Chikorita:
-	loadtrainer LYRA1, LYRA1_9
 .AfterBattle
 	startbattle
 	dontrestartmapmusic
